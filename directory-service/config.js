0c8e5d93908306d5e471d880bbe9237fa244dc01
Tiarnan McGrath <mcgratti@tcd.ie>
2017-12-08 00:52:21 +0000

    Adding gen'd files and models with routes

---
+++ directory-service/config.js
@@ -0,0 +1,22 @@
+/*
+	To be added:
+		httpServer info
+		certs for https
+		Information on other servers 
+		Replica sets
+		Redis store for sessions
+ */
+module.exports={
+	'port': 3000,
+	'file-servers':[
+		/*list of ips*/
+	], 
+	/* redis info for sessions*/
+	'redis':{
+		'port':6379,
+		'host':'127.0.0.1',
+		'socket': '/tmp/redis.sock'
+	},
+	/* https stuff */
+	'credentials': {'key': null, 'cert': null}
+}
