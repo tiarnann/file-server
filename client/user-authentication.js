5e64eee93319dd92f200cd57f715c66d86e902b5
Tiarnan McGrath <mcgratti@tcd.ie>
2017-12-09 20:33:21 +0000

    Fixed login and connect methods. Now storing identityTicket info

--- client/user-authentication.js
+++ client/user-authentication.js
@@ -2,8 +2,9 @@ const authService = require('../lib/authentication/auth-service')
 const fetch = require('node-fetch')
 
 module.exports=(function(auth,fetch){
-	let sessionKey;
-	let ticket;
+	let identityTicketMap = {
+
+	}
 
 	let username;
 	let password;
@@ -17,14 +18,22 @@ module.exports=(function(auth,fetch){
 		password = _password
 
 		const loginMessage = {username, password}
-		console.log(loginMessage)
 
-		const stringifiedPayload = JSON.stringify(loginMessage)
-
-		return fetch(`${this.authUrl}/login`,{
+		const stringifiedPayload = JSON.stringify({username, password})
+		console.log(stringifiedPayload)
+		fetch(`${this.authUrl}/login`,{
 			'method':'POST',
-			'body': loginMessage,
-			'headers':{'content-type': 'application/json'}
+			'body': stringifiedPayload,
+			'headers': {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		})
+		.then(res=>res.text())
+		.then(encrypted=>auth.decrypt(encrypted).with(password))
+		.then(JSON.parse)
+		.then((response)=>{
+			sessionKey = response['session-key']
 		})
 	}
 
@@ -39,17 +48,21 @@ module.exports=(function(auth,fetch){
 
 		const connectMessage = {
 			"username": username,
-			"payload": {sessionKey, identity}
+			"payload": JSON.stringify({sessionKey, identity})
 		}
-
+		console.log({sessionKey, identity})
 		connectMessage.payload = await auth.encrypt(connectMessage.payload).with(password)
 		const stringifiedPayload = JSON.stringify(connectMessage)
 
 
 		return fetch(`${this.authUrl}/connect`,{
 			'method':'POST',
-			'body': connectMessage,
+			'body': stringifiedPayload,
 			'headers':{'content-type': 'application/json'}
+		})
+		.then(res=>res.json())
+		.then((json)=>{
+			identityTicketMap[identity] = json.ticket
 		})
 	}
 
