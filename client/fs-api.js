1873a8967435a3e474aa57dff22591cd09f8e1ff
Tiarnan McGrath <mcgratti@tcd.ie>
2017-12-09 22:18:53 +0000

    Adding soe read and resolve stuff to FSAPI

--- client/fs-api.js
+++ client/fs-api.js
@@ -8,11 +8,40 @@ module.exports=(function(fs,directory){
 		identitySessionKeys[identity] = sessionKey
 	}
 
-	FSAPI.prototype.create = async function(filename, buffer){}
+	FSAPI.prototype.resolve = async function(filename){
+		let arrayBuffer = []
+		const stream = fs.ReadStream(`${directory}/${filename}`)
+		
+		stream.on('data',(chunk)=>{
+			console.log('chunk', chunk)
+			arrayBuffer.push(chunk)
+		})
 
-	FSAPI.prototype.update = async function(filename, buffer){}
+		stream.on('end',()=>{
+			console.log('end',Buffer.from(arrayBuffer))
+			return Buffer.concat(arrayBuffer)
+		})
+	}
 
-	FSAPI.prototype.delete = async function(filename){}
+	FSAPI.prototype.create = async function(filename){
+		console.log('create', filename)
+		this.resolve(filename)
+		.then((fileBuffer)=>{
+			console.log(filename, fileBuffer)
+		})
+	}
+
+	FSAPI.prototype.update = async function(filename){
+		console.log('update', filename)
+		this.resolve(filename)
+		.then((fileBuffer)=>{
+			console.log(filename, fileBuffer)
+		})
+	}
+
+	FSAPI.prototype.delete = async function(filename){
+		console.log('delete', filename)
+	}
 
 	return new FSAPI()
 })
