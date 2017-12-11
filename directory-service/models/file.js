f866ffc26535d90c31923f4f3ad0629af7dda2d2
Tiarnan McGrath <mcgratti@tcd.ie>
2017-12-11 21:36:55 +0000

    Adding modifiedAt and createdAt parts for the file

--- directory-service/models/file.js
+++ directory-service/models/file.js
@@ -4,7 +4,9 @@ module.exports=(function(mongoose){
 	const fileSchema = new Schema({
 		name: String,
 		associatedFileId: String,
-		accessControl:  mongoose.model('AccessControl').schema
+		accessControl:  mongoose.model('AccessControl').schema,
+		createdAt: {type: Date, default:Date.now()},
+		modifiedAt: {type: Date, default:''}
 	}, { collection: 'files' })
 
 	return mongoose.model('File', fileSchema) 
