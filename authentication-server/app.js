afa24966a6f1196b470c7964702db1bd0bd0972e
Tiarnan McGrath <mcgratti@tcd.ie>
2017-12-12 15:27:56 +0000

    Connected up baseline server

--- authentication-server/app.js
+++ authentication-server/app.js
@@ -1,15 +1,25 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const config = require('./config')
+const db = require('mongodb').MongoClient
+const app = express();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var api = require('./routes/api');
+/* Connect to database */
+db.connect(config.db,()=>{
+	console.log('Connected to database.')
+})
 
-var app = express();
+
+
+/* Routes */
+const index = require('./routes/index');
+const users = require('./routes/users');
+const api = require('./routes/api')(db);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +30,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+/* Checking for database */
+app.use((req, res, next)=>{
+	const databaseConnnectionMissing = db.isConnected
+	if(databaseConnnectionMissing){
+	  const err = new Error('no database connection found');
+	  err.status = 500;
+	  next(err);
+	}
+	next()
+});
 
 app.use('/', index);
 app.use('/users', users);
@@ -33,7 +54,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next)=>{
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- authentication-server/routes/api.js
+++ authentication-server/routes/api.js
@@ -1,36 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const user = require('../schemas/user')
 
+const api = (function(express, userSchema, db){
+	const router = express.Router();
 
-const auth = (req,res, next)=>{
-	return true
-}
+	const auth = (req,res, next)=>{
+		next()
+	}
 
-router.post('login',(req,res, next)=>{
-	const {username, payload} = req.body
+	router.post('login', auth,(req,res, next)=>{
+		const {username, payload} = req.body
 
-	// lookup associated password
-	// check if session-key exists generate session-key
-	// 
-	res.send({});
+		// lookup associated password
+		// check if session-key exists generate session-key
+		res.send({});
+	})
+
+	router.post('signup',(req,res, next)=>{
+		const {username, payload} = req.body
+
+		// lookup associated password
+		// check if session-key exists generate session-key
+		// 
+		res.send({});
+	})
+
+	router.get('connect', auth,(req,res, next)=>{
+		const {username, payload} = req.body
+
+		// lookup associated password
+		// check if session-key exists generate session-key
+		// 
+		res.send({});
+	})
+
+	return router
 })
 
-router.post('signup',(req,res, next)=>{
-	const {username, payload} = req.body
-
-	// lookup associated password
-	// check if session-key exists generate session-key
-	// 
-	res.send({});
-})
-
-router.get('connect', auth,(req,res, next)=>{
-	const {username, payload} = req.body
-
-	// lookup associated password
-	// check if session-key exists generate session-key
-	// 
-	res.send({});
-})
-
-module.exports = router;
+module.exports = api.bind(null, express, user)
