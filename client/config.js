ba9cc9ad26b64d001e27b11f1d5593e2a8cd432f
Tiarnan McGrath <mcgratti@tcd.ie>
2017-12-12 18:04:22 +0000

    Removing uecessary stuff

--- client/config.js
+++ client/config.js
@@ -5,12 +5,3 @@ module.exports={
 
 	}
 }
-
-/*
-AUTH_HOST='http://localhost/api'
-FILE_HOST='http://localhost/api'
-DIRECTORY_HOST='http://localhost/api'
-AUTH_PORT=3000
-FILE_PORT=3001
-DIRECTORY_PORT=3002
-*/
